a6faeab714f5f40f945a2bc681c3d2e9fd25a6de
Nikola <nikola.kiki.00@gmail.com>
2021-07-05 01:21:10 +0200

    Sitne prepravke i privodjenje projekta kraju

--- OSAProjekat/src/main/java/com/example/OSAProjekat/controller/AdministratorController.java
+++ OSAProjekat/src/main/java/com/example/OSAProjekat/controller/AdministratorController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +15,8 @@ import com.example.OSAProjekat.model.dto.AdministratorDTO;
 import com.example.OSAProjekat.model.entity.Administrator;
 import com.example.OSAProjekat.service.AdministratorService;
 
-
 @RestController
+@CrossOrigin(origins="http://localhost:3000")
 @RequestMapping(value = "api/admini")
 public class AdministratorController {
 
